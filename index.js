246fe89f249b3ee9511d85c58d6d45634bb4f38d
James Li <jamespeilunli@gmail.com>
2022-02-08 21:07:52 -0800

    add samurai talking at start

--- index.js
+++ index.js
@@ -16,6 +16,19 @@ function fill_text(text, x, y, font_sz, color="black", font="sans-serif") {
     ctx.closePath();
 }
 
+function popup(text, x, y, width, height="auto", font_sz=14, color="black", bg_color="white", font="sans-serif", padding=10) {
+    let p = document.getElementById("text");
+    p.setAttribute("style", `margin: ${y+8}px ${x+8}px; width: ${width}px; height: ${height}px; padding: ${padding}px;
+        font-size: ${font_sz}px; color: ${color}; background-color: ${bg_color}; font-family: ${font};`)
+    p.innerHTML = text;
+}
+
+function remove_popup() {
+    let p = document.getElementById("text");
+    p.innerHTML = "";
+    p.setAttribute("style", "");
+}
+
 function between(val, low, high) {
     if (typeof val === "object") { // checking if point is between 2 points
         return between(val[0], low[0], high[0]) && between(val[1], low[1], high[1]);
@@ -34,12 +47,13 @@ function randint(min, max) {
 class Entity {
     x; y; dx; dy; r; color;
 
-    constructor(x, y, dx=0, dy=0, r=10, color="blue") {
+    constructor(x, y, dx=0, dy=0, r=10, label=null, color="blue") {
         this.x = x;
         this.y = y;
         this.dx = dx;
         this.dy = dy;
         this.r = r;
+        this.label = label;
         this.color = color;
     }
 
@@ -49,6 +63,10 @@ class Entity {
         ctx.fillStyle = this.color;
         ctx.fill();
         ctx.closePath();
+
+        if (this.label) {
+            fill_text(this.label, this.x - parseInt(this.label.length/3*10), this.y - 20, 14, "white");
+        }
     }
 
     tick() {
@@ -57,18 +75,25 @@ class Entity {
 
         this.draw();
     }
+
+    say(text, unsay=false) {
+        if (unsay) { remove_popup(); return; }
+        popup(text, this.x, this.y, 250);
+    }
+
 }
 
 class Player extends Entity {
     x; y; dx; dy; r; color;
 
-    constructor(x, y, dx=0, dy=0, r=10, color="lightblue") {
+    constructor(x, y, dx=0, dy=0, r=10, label=null, color="lightblue") {
         super();
         this.x = x;
         this.y = y;
         this.dx = dx;
         this.dy = dy;
         this.r = r;
+        this.label = label;
         this.color = color;
     }
 }
@@ -125,88 +150,16 @@ class Area {
 
 }
 
-function popup(text, x, y, width, height, font_sz, color="black", bg_color="white", font="sans-serif", padding=10) {
-    let p = document.getElementById("text");
-    p.setAttribute("style", `margin: ${y+8}px ${x+8}px; width: ${width}px; height: ${height}px; padding: ${padding}px;
-        font-size: ${font_sz}px; color: ${color}; background-color: ${bg_color}; font-family: ${font};`)
-    p.innerHTML = text;
-}
-
-function remove_popup() {
-    let p = document.getElementById("text");
-    p.innerHTML = "";
-}
-
-
-// PLANT STUFF
-/*
-class Plant {
-    constructor(x, y, img_names) {
-        this.x = x;
-        this.y = y;
-        this.img_names = img_names;
-        this.stage = 0;
-
-        // idk what these are
-        this.width = 50;
-        this.height = 50; 
-    }
-
-    draw() {
-        ctx.drawImage(this.img_names[this.stage], this.x, this.y);
-    }
-
-    grow() {
-        if (this.stage !== this.img_names.length - 1 && randint(1, 100) === 1) {
-            this.stage++;
-        }
-    }
-}
-
-class Plot {
-    
-    constructor(x, y, width, height, color, plants) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.color = color;
-        this.plants = plants;
-    }
-    
-    draw() {
-        for (let plant of this.plants) {
-            plant.draw();
-        }
-    }
-
-    detect_collect(x, y) {
-        for (let plant of this.plants) {
-            if (between([x, y], [plant.x, plant.y], [plant.x+plant.width, plant.y+plant.height])) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    grow() {
-        for (let plant of this.plants) {
-            plant.grow();
-        }
-    }
-        
-}
-*/
-
 // MAIN
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-let player = new Player(25, 25, 0, 0);
+let player = new Player(500, 300, 0, 0, 10, "Player");
+let samurai1 = new Entity(50, 225, 0, 0, 10, "Samurai 陽翔", "red");
 
 let tmp_area = new Area([], "blue", null, null, null, null);
-let cur_area = new Area([], "#C4A484", tmp_area, null, null, null, "volcanic_landscape.jpg");
-tmp_area.south = cur_area;
+let cur_area = new Area([], "#C4A484", null, tmp_area, null, null, "volcanic_landscape.jpg");
+tmp_area.west = cur_area;
 
 function tick() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -215,7 +168,14 @@ function tick() {
     if (cur_area.change_area(player.x, player.y, player.r)) {
         cur_area = cur_area.change_area(player.x, player.y, player.r);
         if (!between(player.y, 10 + player.r, canvas.height - 10 - player.r)) player.y = canvas.height - player.y;
-        if (!between(player.x, 10 + player.r, canvas.width - 10 - player.r)) player.x = canvas.width - player.y;
+        if (!between(player.x, 10 + player.r, canvas.width - 10 - player.r)) player.x = canvas.width - player.x;
+
+        remove_popup();
+    }
+
+    if (cur_area.bg_color === "#C4A484") {
+        samurai1.say("Hey peasant! What are you doing here? Don't you know that because of mountains, only about 20% of land can be farmed! Go find some land that you can actually farm to make yourself useful!")
+        samurai1.tick();
     }
 
     player.tick();
