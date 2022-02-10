8d6ecbf00a18e5913167ad8919a866c3a09bc3da
James Li <jamespeilunli@gmail.com>
2022-02-09 21:23:38 -0800

    add shintoist

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function fill_text(text, x, y, font_sz, color="black", font="sans-serif") {
     ctx.closePath();
 }
 
-function popup(text, x, y, width, height="auto", font_sz=14, color="black", bg_color="white", font="sans-serif", padding=10) {
+function popup(text, x, y, width, height="auto", font_sz=12, color="black", bg_color="white", font="sans-serif", padding=10) {
     let p = document.getElementById("text");
     p.setAttribute("style", `margin: ${y+8}px ${x+8}px; width: ${width}px; height: ${height}px; padding: ${padding}px;
         font-size: ${font_sz}px; color: ${color}; background-color: ${bg_color}; font-family: ${font};`)
@@ -46,7 +46,7 @@ function randint(min, max) {
 // ENTITY CLASS
 class Entity {
 
-    constructor(x, y, dx=0, dy=0, img, label=null, img_width=0, img_height=0) {
+    constructor(x, y, dx=0, dy=0, img, label=null, img_width=0, img_height=0, label_color="white") {
         this.x = x;
         this.y = y;
         this.dx = dx;
@@ -58,13 +58,15 @@ class Entity {
 
         this.img_width = img_width;
         this.img_height = img_height;
+
+        this.label_color = label_color;
     }
 
     draw() {
         ctx.drawImage(this.img, this.x, this.y);
 
         if (this.label) {
-            fill_text(this.label, this.x, this.y-5, 14, "white");
+            fill_text(this.label, this.x, this.y-5, 12, this.label_color);
         }
     }
 
@@ -76,7 +78,7 @@ class Entity {
     }
 
     say(text) {
-        popup(text, this.x+this.img_width, this.y+this.img_height, 250);
+        popup(text, this.x+this.img_width/1.5, this.y+this.img_height/1.5, 250);
     }
 
 }
@@ -179,7 +181,7 @@ class Area {
         this.is_denied_change_area = true;
         samurai2.x = x;
         samurai2.y = y;
-        samurai2.say("Hey you! You can't leave this land! You're a PEASANT. You have to farm in the land your Daimyo told you to farm in. You should know the social hierarchy! Emperor > Shogun > Daimyo > Samurai > Ronin > Peasants > Artisans > Merchants. You're waaaaay at the bottom.");
+        samurai2.say("Hey you! You can't leave this land! You're a PEASANT. You have to farm in the land your Daimyo told you to farm in. You should know the social hierarchy! Emperor > Shogun > Daimyo > Samurai > Ronin > Peasants > Artisans > Merchants. I know it's kind of unfair, but that's the feudal system. It has advantages, like good organization of classes, but also has disadvantages, like inequality between classes.");
         draw_samurai2 = true;
     }
 
@@ -191,7 +193,8 @@ const ctx = canvas.getContext("2d");
 
 let player = new Entity(500, 300, 0, 0, "japanese_peasant.png", "Player", 60, 95);
 let samurai1 = new Entity(50, 225, 0, 0, "samurai.png", "Samurai 陽翔", 60, 62);
-let samurai2 = new Entity(-10, -10, 0, 0, "samurai.png", "Samurai 陽菜", 60, 62);
+let samurai2 = new Entity(-100, -100, 0, 0, "samurai.png", "Samurai 陽菜", 60, 62);
+let shintoist = new Entity(-100, -100, 0, 0, "japanese_peasant.png", "Shintoist 哲也", 60, 95, "black");
 
 let draw_samurai2 = false;
 
@@ -205,7 +208,6 @@ let cur_area = new Area([], "#C4A484", null, tmp_area, null, null, "volcanic_lan
 tmp_area.west = cur_area;
 tmp_area.north = tmp2_area;
 
-samurai2.tick();
 function tick() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     cur_area.background();
@@ -213,9 +215,23 @@ function tick() {
 
     if (cur_area.bg_color === "#C4A484") {
         if (!cur_area.is_denied_change_area) {
-            samurai1.say("Hey peasant! What are you doing here? Don't you know that because of mountains, only about 20% of land can be farmed! Go find some land that you can actually farm to make yourself useful!")
+            samurai1.say("Hey peasant! What are you doing here? Don't you know that because of mountains mostly caused by the 188 volcanoes in Japan, only about 20% of land can be farmed! Go right and you'll find some land you can farm in. You should be greatful your Daiymo gave you land. Armies often fight over little patches of fertile farmland!")
         }
         samurai1.tick();
+    }
+
+    if (draw_samurai2) {
+        samurai2.tick();
+    }
+
+    if (cur_area.bg_color === "yellow") {
+        shintoist.x = 50;
+        shintoist.y = 50;
+        shintoist.tick();
+        shintoist.say("Hello! I haven't seen you before. Let me introduce you to the religion of Shinto. Us shintoists believe that all natural beings are alive, and have spirits. We ask the kami, the nature spirits, for help when we need it. To honor the kami, we worship at Shinto shrines.");
+    } else {
+        shintoist.x = -100;
+        shintoist.y = -100;
     }
 
     if (cur_area.change_area(player.x, player.y, player.img_width, player.img_height)) {
